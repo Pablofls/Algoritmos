a0b120aa523e1647726e1c6778ad4f321b649841
David Muñoz Zavala <david@Davids-MacBook-Pro.local>
2025-08-15 12:40:01 -0600

    creacion de documento

---
+++ tensor.cpp
@@ -0,0 +1,15 @@
+// Tensor
+// Programar un arreglo de grado 3, 3x3x3, en el que la entrada de cada casilla es la 
+//suma de los iteradores 
+
+
+for(int i = 0; i < x; i++){
+    for (int j = 0; j < x; j++){
+        for int k = 0; k < x; k++{
+
+        }    }
+}
+
+0 3 0
+0 0 0 
+0 0 0 
