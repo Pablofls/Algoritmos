722ea65c2f6183844b7477af487e5655631b8a68
Pablofls <pablo.flsrdz@gmail.com>
2025-08-15 12:49:54 -0600

    ADD tensor pablo

--- tensorPablo.cpp
+++ tensorPablo.cpp
@@ -6,7 +6,7 @@ int main(){
 
     for(int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            for int k = 0; k < 3; k++{
+            (for int k = 0; k < 3; k++){
                 arr[i][j][k] = i + j + k;
             }    
         }
