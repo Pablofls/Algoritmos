6dfbdecf66b03b22e1c9effaffe8b4ea2b264ef5
Pablofls <pablo.flsrdz@gmail.com>
2025-08-19 12:03:21 -0600

    ADD bubble sort

---
+++ bubble.cpp
@@ -0,0 +1,40 @@
+/*
+Pablo Flores 611194
+David Muñoz
+Ingeniería en Tecnologías Computacionales
+Análisis y Diseño de Algoritmos
+19 de agosto, 2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[9] = {9, 1, 8, 2, 7, 6, 3, 4, 5};
+    int temp;
+    
+    cout << "Arreglo inicial: \n";
+    
+    for(int x=0; x < 9; x++){
+        cout << arr[x] << " ";
+    } 
+
+    for(int i=0; i < 9 - 1; i++){
+        for (int j=0; j < 9 - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j +1];
+                arr[j + 1] = temp;
+
+            }
+        }
+    }
+
+    cout << "Arreglo final: \n";
+    
+    for(int x=0; x < 9; x++){
+        cout << arr[x] << " ";
+    } 
+
+}
