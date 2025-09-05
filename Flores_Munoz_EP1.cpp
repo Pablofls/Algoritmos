ea742b983e740bece72bfb7627609230dad9d803
Pablofls <pablo.flsrdz@gmail.com>
2025-09-05 12:02:32 -0600

    ADD examen

---
+++ Flores_Munoz_EP1.cpp
@@ -0,0 +1,16 @@
+/*
+Ingeniería en Tecnologías Computacionales
+Análisis y Diseño de Algoritmos
+Pablo Flores 611194
+David Muñoz 621613
+8 de septiembre, 2025
+*/
+
+
+/*
+Complejidad temporal: O(n), siendo n el multiplicador, por lo que se repetirá la suma n número de veces.
+Complejidad espacial: También es O(n), ya que el espacio usado es proporcional al número de veces que se llama a la función recursiva.
+
+
+Nosotros damos nuestra palabra que hemos realizado esta actividad con integridad académica.
+*/
