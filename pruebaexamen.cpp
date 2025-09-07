d4a7dff79f7909040590bf3c2522696ad0b75ab3
David Muñoz Zavala <david@Davids-MacBook-Pro.local>
2025-09-07 05:15:53 -0600

    le meti comentarios

--- pruebaexamen.cpp
+++ pruebaexamen.cpp
@@ -9,6 +9,8 @@ David Muñoz 621613
 #include <vector>
 using namespace std;
 
+
+//This process is going to include de digits of the number in an array and sort it out in reverse so we get the digits from the greater to the lowest.
 void sortMaxtoMin(string number, vector<int>& arrayMax){
     for(int i = 0; i < number.length(); i++){
         arrayMax.push_back(number[i] - '0');
@@ -17,6 +19,7 @@ void sortMaxtoMin(string number, vector<int>& arrayMax){
 }
 
 
+//This process is going to include de digits of the number in an array and sort it out in so we get the digits from the lowest to the greatest.
 void sortMintoMax(string number, vector<int>& arrayMin){
     for(int i = 0; i < number.length(); i ++){
         arrayMin.push_back(number[i] - '0');
@@ -25,6 +28,8 @@ void sortMintoMax(string number, vector<int>& arrayMin){
     
 }
 
+//Here we are going to get the number in his greatest form by multiplying each digit on the array by mult, which first is going to be 1000, then 100... 
+//So that we get the integer number
 void getMax(vector<int>& arrayMax, int &max, int &mult1){
     for(int i = 0; i < arrayMax.size(); i++){
         int result = mult1 * arrayMax[i];
@@ -33,6 +38,8 @@ void getMax(vector<int>& arrayMax, int &max, int &mult1){
     }
 }
 
+//Here we are going to get the number in his lowest form by multiplying each digit on the array by mult, which first is going to be 1000, then 100... 
+//So that we get the integer number
 void getMin(vector<int>& arrayMin, int &min, int &mult2){
     for(int i = 0; i < arrayMin.size(); i ++){
         int result = mult2 * arrayMin[i];
@@ -41,7 +48,19 @@ void getMin(vector<int>& arrayMin, int &min, int &mult2){
     }
 }
 
+//This is the main function, is recursive and it is going to repeat unless we get the number to be "6174". 
+//First, we verify that all the digits in the number given are not thr same
+//Then, we call to all the functions done before and clear the arrays so that if we have to repeat the process, ther are not old digits of the old number.
+//We get the final result of the number, if it is not the number wanted, we are going to add 1 to the variable "count", and declare the other variables used.
+//Finally, the function is going to call itself back again until we reach the number.
 int getFinal(int final, int max, int min, int count, vector<int>& arrayMin, vector<int>& arrayMax, string number, int &mult1, int &mult2){
+    for(int i = 0; i < number.length(); i++){
+        if(number[i] == number[i+1] && number[i] == number[i+2] && number[i] == number[i+3]){
+            cout << "All the numbers are the same, please introduce another number" << endl;
+            return 0;
+        }
+    }
+    
     sortMaxtoMin(number, arrayMax);
     sortMintoMax(number, arrayMin);
     getMax(arrayMax, max, mult1);
@@ -65,7 +84,7 @@ int getFinal(int final, int max, int min, int count, vector<int>& arrayMin, vect
 }
 
 int main(){
-string number = "4782";
+string number = "1111";
 vector <int> arrayMax;
 vector <int> arrayMin;
 int mult1 = 1000; 
