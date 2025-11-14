33759c5a6260db3b2d4eef9e43cfe4b3dee515f8
Pablofls <pablo.flsrdz@gmail.com>
2025-11-14 10:12:55 -0600

    coments

--- backtracking.cpp
+++ backtracking.cpp
@@ -1,9 +1,18 @@
+/*
+Ingeniería en Tecnologías Computacionales
+Análisis y Diseño de Algoritmos
+Pablo Flores 611194
+David Muñoz 621613
+14 de Noviembre, 2025
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <limits>
 using namespace std;
 
-
+//inicializar la mejor solucion con el menor numero posible
 int mejorValor = numeric_limits<int>::min();
 int n, weight;
 
@@ -13,10 +22,14 @@ vector<int> mejorSolucion;
 
 void Backtracking(int i, int pesoActual, int valorActual, vector<int> & solucionActual){
 
+    // Si ya nos pasamos de la capacidad, terminamos esta rama
     if (pesoActual > weight) 
         return;
 
+    // Si ya evaluamos todos los objetos
     if (i == n){
+
+        // Si el valor de esta combinación es mejor que el mejor encontrado
         if (valorActual > mejorValor){
 
             mejorValor = valorActual;
@@ -26,9 +39,12 @@ void Backtracking(int i, int pesoActual, int valorActual, vector<int> & solucion
         return;
     }
 
+    // Opción 1: Incluir el objeto i
     solucionActual.push_back(i); 
     Backtracking(i + 1, pesoActual + peso[i], valorActual + valor[i], solucionActual);
     solucionActual.pop_back();
+
+    // Opción 2: NO incluir el objeto i
     Backtracking(i + 1, pesoActual, valorActual, solucionActual);
 }
 
@@ -71,3 +87,11 @@ int main(){
 
     return 0;
 }
+
+
+/*
+We hereby affirm that we have done this activity with academic integrity.
+
+References
+OpenAI. (2025). ChatGPT (versión GPT-5) [Modelo de lenguaje grande]. https://chat.openai.com/
+*/
